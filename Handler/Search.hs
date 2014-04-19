d416a9dfd476b1dd1ab4edfff27803e9fd846a77
Zac Slade <krakrjak@gmail.com>
2014-04-19 04:02:57 -0500

    Add in search widget...

---
+++ Handler/Search.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE TupleSections, OverloadedStrings #-}
+module Handler.Search where
+
+import Import
+
+data Result = Result
+  { resultTitle :: Text }
+
+getSearchR :: Handler Html
+getSearchR = do
+    ((formRes, searchWidget), _) <- runFormGet searchForm
+    searchResults <-
+      case formRes of
+        FormSuccess qstring -> getResults qstring
+        _ -> return []
+    defaultLayout $ do
+        aDomId <- newIdent
+        setTitle "Searching for Stutors"
+        $(widgetFile "search")
+
+searchForm :: Html -> MForm Handler (FormResult Text, Widget)
+searchForm = renderDivs $ areq (searchField True) "Search" Nothing
+
+getResults::Text -> Handler [Result]
+getResults qstring = return [ Result "Joe Brown", Result "Jessica Simpson" ]
